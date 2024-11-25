eb03075028642b278df3bec72d2b7cf5de353c1b
SamuraiCali <xavierw.work@gmail.com>
2024-11-25 12:38:40 -0500

    MC PT1.3

--- app/prototype/login/page.tsx
+++ app/prototype/login/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { Input } from "@nextui-org/react";
+import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function LoginPage() {
